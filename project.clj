7654ff23918852c7f51de835a457c98e53cc1684
Chris Dean <ctdean@standardtreasury.com>
2014-11-04 15:49:15 -0800

    Version foe using git-describe
    Test Plan: lein install
    
    Reviewers: keith, mike, jim, david
    
    Reviewed By: david
    
    Subscribers: Lighthouse, jim
    
    Differential Revision: http://phabricator.corp.standardtreasury.com/D3699

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject foe "0.1.0"
+(defproject foe
+  ;; The version string is ignored in favor of the git tag that starts
+  ;; with 'v'.
+  "0.2.0-tag"
   :description "Foe: Flexible authentication and authorization"
   :url "http://standardtreasury.com/foe"
   :license {:name "Eclipse Public License"
@@ -13,6 +16,7 @@
   :plugins [[lein-ancient "0.5.5"]
             [s3-wagon-private "1.1.2"]
             [jonase/eastwood "0.1.4"]
-            [lein-test-out "0.3.1"]]
+            [lein-test-out "0.3.1"]
+            [org.clojars.cvillecsteele/lein-git-version "1.0.2"]]
   :profiles {:dev {:dependencies [[compojure "1.1.8"]
                                   [ring-mock "0.1.5"]]}})
