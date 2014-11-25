78a6ff68c0882ae3326c8e04d83e5ed82410d482
Keith Ballinger <keith@standardtreasury.com>
2014-11-25 14:07:05 -0800

    [foe] Handle errors better
    Summary: Handles redirecting or 401'ing depending on allow-anonymous and whitelist. Also, more comments.
    
    Test Plan: lein test
    
    Reviewers: #standard_treasury, david
    
    Reviewed By: #standard_treasury, david
    
    Subscribers: david, Lighthouse
    
    Differential Revision: http://phabricator.corp.standardtreasury.com/D3889

--- src/foe/authentication.clj
+++ src/foe/authentication.clj
@@ -10,15 +10,16 @@
    :body   message})
 
 (defn- redirect-or-401
-  [handler request allow-anonymous redirect-url whitelist]
-  (if (or allow-anonymous
-          (whitelist (req/path-info request)))
+  [handler request allow-anonymous redirect-url message]
+  (if allow-anonymous
       (handler request)
       (if redirect-url
           (resp/redirect redirect-url)
-          (respond-401 "Unauthorized"))))
+          (respond-401 message))))
 
 (defn session-auth-fn
+  "The session-auth-fn function will attempt to authenticate a request using the session. If the
+   session contains a :user with :role and :name, authentication was successful."
   [request]
   (let [session (:session request)
         user    (:user session)]
@@ -26,16 +27,28 @@
              (contains? user :roles))
         user)))
 
-(defn wrap-authentication [handler auth-fn &
-                           {:keys [allow-anonymous redirect-url whitelist]
-                            :or {allow-anonymous false
-                                 redirect-url nil
-                                 whitelist #{}}}]
+(defn wrap-authentication
+  "The wrap-authentication function attempts to authenticate the HTTP request using the supplied
+   function auth-fn. If the authentication fails, a 401 will be returned, unless:
+       - the request is whitelisted by the whitelist function, OR
+       - allow-anonymous is true.
+
+   When authentication is sccuessful, the request will include a new :user key, as supplied by the
+   auth-fn function.
+
+   The auth-fn function should return a map representing the authenticated user. When used with
+   foe.authorization, this user map should include a :name and :roles key."
+  [handler auth-fn & {:keys [allow-anonymous redirect-url whitelist]
+                      :or   {allow-anonymous false
+                             redirect-url nil
+                             whitelist #{}}}]
   (fn [request]
-    (slingshot/try+
-      (let [user (auth-fn request)]
-        (if user
-          (handler (assoc request :user user))
-          (redirect-or-401 handler request allow-anonymous redirect-url whitelist)))
-      (catch [:type :foe.exceptions/failed-auth] {:keys [message]}
-        (respond-401 message)))))
+    (if (whitelist (req/path-info request))
+      (handler request))
+      (slingshot/try+
+        (let [user (auth-fn request)]
+          (if user
+            (handler (assoc request :user user))
+            (redirect-or-401 handler request allow-anonymous redirect-url "Unauthorized")))
+        (catch [:type :foe.exceptions/failed-auth] {:keys [message]}
+          (redirect-or-401 handler request allow-anonymous redirect-url message)))))
