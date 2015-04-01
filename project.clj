522d1b93080716a21aaae936544c504430b2d5fd
Chris Dean <ctdean@standardtreasury.com>
2015-04-01 14:18:36 -0700

    Inc version for release

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject foe
-  ;; The version string is ignored in favor of the git tag that starts
-  ;; with 'v'.
-  "0.2.0-tag"
+  "0.3.0"
   :description "Foe: Flexible authentication and authorization"
   :url "http://standardtreasury.com/foe"
   :license {:name "Eclipse Public License"
@@ -9,15 +7,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.1"]
                  [clj-http "1.0.0"]]
-  :repositories
-    {"internal" {:url "s3://standard-releases/releases/"
-                 :username :env/aws_access_key_id
-                 :passphrase :env/aws_secret_access_key
-                 :sign-releases false}}
-  :plugins [[lein-ancient "0.5.5"]
-            [lein-maven-s3-wagon "0.2.4"]
-            [jonase/eastwood "0.1.4"]
-            [lein-test-out "0.3.1"]
-            [org.clojars.cvillecsteele/lein-git-version "1.0.2"]]
+  :plugins [[jonase/eastwood "0.1.4"]
+            [lein-test-out "0.3.1"]]
   :profiles {:dev {:dependencies [[compojure "1.1.8"]
                                   [ring-mock "0.1.5"]]}})
